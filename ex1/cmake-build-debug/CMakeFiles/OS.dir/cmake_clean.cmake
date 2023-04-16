a297a40953518331ca4056856fa9d2c73c61c9e9
shirlevy007 <101273140+shirlevy007@users.noreply.github.com>
2023-04-16 14:06:21 +0300

    Add files via upload

---
+++ ex1/cmake-build-debug/CMakeFiles/OS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS.dir/main.cpp.o"
+  "OS"
+  "OS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ex1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "osm.cpp"
+
+int main ()
+{
+
+  osm_function_time (10);
+  osm_operation_time (10);
+  osm_syscall_time (10);
+}
