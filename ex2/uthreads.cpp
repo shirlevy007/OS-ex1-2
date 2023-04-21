ce243eef2c8192c6950cecc425f749add0a5a09d
shirlevy <shir.levy2@mail.huji.ac.il>
2023-04-21 16:20:21 +0300

    all funcs written never tested

--- ex2/uthreads.cpp
+++ ex2/uthreads.cpp
@@ -52,16 +52,50 @@ enum SigCase {
     SIGSLEEP=SIGVTALRM+1, SIGTERMINATE=SIGVTALRM+2, SIG_BLOCKSLF=SIGVTALRM+3
 };
 
+#ifdef __x86_64__
+/* code for 64 bit Intel arch */
+
+typedef unsigned long address_t;
+#define JB_SP 6
+#define JB_PC 7
+
 /* A translation is required when using an address of a variable.
    Use this as a black box in your code. */
-address_t translate_address(address_t addr) {
-  address_t ret;
-  asm volatile("xor    %%gs:0x18,%0\n"
-               "rol    $0x9,%0\n"
-  : "=g" (ret)
-  : "0" (addr));
-  return ret;
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%fs:0x30,%0\n"
+                 "rol    $0x11,%0\n"
+            : "=g" (ret)
+            : "0" (addr));
+    return ret;
 }
+
+#else
+/* code for 32 bit Intel arch */
+
+typedef unsigned int address_t;
+#define JB_SP 4
+#define JB_PC 5
+
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+//address_t translate_address(address_t addr)
+//{
+//    address_t ret;
+//    asm volatile("xor    %%gs:0x18,%0\n"
+//                 "rol    $0x9,%0\n"
+//    : "=g" (ret)
+//    : "0" (addr));
+//    return ret;
+//}
+
+
+#endif
+
+#define SECOND 1000000
+#define STACK_SIZE 4096
 
 ///------------------------------------------- Thread ------------------------------------------------------
 
@@ -108,12 +142,12 @@ class Thread {
         return true;
     }
     void setup_thread(){
-        address_t sp = (address_t) this->stack + STACK_SIZE - sizeof(address_t);
-        address_t pc = (address_t) this->entry_point;
-        sigsetjmp(this->env, 1);
-        (this->env->__jmpbuf)[JB_SP] = translate_address(sp);
-        (this->env->__jmpbuf)[JB_PC] = translate_address(pc);
-        if (sigemptyset(&this->env->__saved_mask)== EXIT_FAIL){
+        address_t sp = (address_t) stack + STACK_SIZE - sizeof(address_t);
+        address_t pc = (address_t) entry_point;
+        sigsetjmp(env, 1);
+        (env->__jmpbuf)[JB_SP] = translate_address(sp);
+        (env->__jmpbuf)[JB_PC] = translate_address(pc);
+        if (sigemptyset(&env->__saved_mask)== EXIT_FAIL){
             std::cerr << SIGEMPTYSET_FAILED << std::endl;
             exit(1);
         }
